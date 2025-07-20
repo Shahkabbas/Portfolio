96efffa2393cff300a72821a9e55c02ae3de2902
Shah Kashif Abbas <shahkabbas@gmail.com>
2025-07-20 17:02:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -100,9 +100,8 @@ export default function Portfolio() {
           }}
         />
       </div>
-      </div>
-
-      /* Progress Bar */
+      
+      {/* Progress Bar */}
       <motion.div
         className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-blue-500 to-purple-600 z-50 origin-left"
         style={{ scaleX: scrollYProgress }}
